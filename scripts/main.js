2b9770db74fd0eb023468347f5a379ec518b80fa
Kieran Utz <Kieranju@gmail.com>
2013-11-28 13:48:23 -0800

    Insane Amounts of JQuery Animation
    This is just getting crazy. I'm also very hungry.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var dropSequence = false;
+var animSequence = false;
 var branchSelected = false;
 
 var htmldrop = $("#htmldrop");
@@ -25,11 +25,11 @@ $(document).ready(function() {
     }
     
     function dropDown(drop) {
-        if (dropSequence == false)
+        if (animSequence == false)
         {
-            dropSequence = true;
-            menu = drop.children("ul");
-            displacement = (menu.children("li").length) * 26;
+            animSequence = true;
+            var menu = drop.children("ul");
+            var displacement = (menu.children("li").length) * 26;
             
             if (menu.css("opacity") == "0")
             {
@@ -38,7 +38,7 @@ $(document).ready(function() {
                     menu.css("position", "static");
                     drop.css("margin-bottom", "0px");
                     menu.animate({opacity: "1"}, 300, function() {
-                        dropSequence = false;
+                        animSequence = false;
                     });
                 });
             }
@@ -46,7 +46,7 @@ $(document).ready(function() {
             {
                 drop.children("h2").toggleClass("toggle");
                 menu.animate({opacity: "0"}, 175, function() {
-                    dropSequence = false;
+                    animSequence = false;
                     menu.css("position", "absolute");
                     drop.css("margin-bottom", displacement + "px");
                     drop.animate({"margin-bottom": "0px"}, 360, "easeInOutCubic");
@@ -56,37 +56,98 @@ $(document).ready(function() {
     }
     
     $("#sidebar").find("a").click(function() {
-        activeObj = $(this);
-        objLocation = ((activeObj.position().top) + 205).toString();
-        
-        if (branchSelected == false)
+        if (animSequence == false)
         {
-            activeObj.toggleClass("selected");
-            branchSelected = true;
-            branchObj = activeObj;
-        }
-        else
-        {
-            if (activeObj.hasClass("selected"))
+            var activeObj = $(this);
+            var objLocation = ((activeObj.position().top) + 205);
+            animSequence = true;
+            
+            if (branchSelected == false) // Nothing is selected, button is pressed
             {
                 activeObj.toggleClass("selected");
-                delete branchObj;
-                branchSelected = false;
+                branchSelected = true;
+                branchObj = activeObj;
+                pageRender(objLocation, true, false);
             }
             else
             {
-                branchObj.toggleClass("selected");
-                activeObj.toggleClass("selected");
-                branchObj = activeObj;
+                if (activeObj.hasClass("selected")) // Button is selected, same button gets pressed
+                {
+                    activeObj.toggleClass("selected");
+                    delete branchObj;
+                    branchSelected = false;
+                    pageRender(objLocation, false, true);
+                }
+                else // Button is selected, different button gets pressed
+                {
+                    branchObj.toggleClass("selected");
+                    activeObj.toggleClass("selected");
+                    branchObj = activeObj;
+                    pageRender(objLocation, false, false);
+                }
             }
         }
-        
-        $("main").append("<hr id='lineGen_01' />");
-        $("#lineGen_01").css("top", objLocation + "px");
     });
     
-    function pageRender(drop) {
+    function pageRender(objLocation, newLine, shouldReset) {
         
+        if (newLine == true)
+        {
+            $("main").append("<hr id='lineGen_01' />");
+            var lineGen_01 = $("#lineGen_01");
+            lineGen_01.css("top", objLocation.toString() + "px");
+            lineGen_01.delay(200).animate({"width": "75px", "opacity": "1"}, 150, "easeInOutQuart", function() {
+                $("main").append("<div id='lineGen_02'></div>");
+                var lineGen_02 = $("#lineGen_02");
+                lineGen_02.css({"top": (objLocation + 4).toString() + "px"});
+                lineGen_02.animate({"width": "9px", "height": "9px", "opacity": "1"}, 150, "easeInOutBack", function() {
+                    $("main").append("<div id='lineGen_03'></div>");
+                    var lineGen_03 = $("#lineGen_03");
+                    lineGen_03.css({"height": (objLocation - 75).toString() + "px", "top": "80px"});
+                    lineGen_03.animate({"opacity": "1"}, 150, "easeInOutQuart", function() {
+                        $("main").append("<div id='lineGen_04'></div>");
+                        var lineGen_04 = $("#lineGen_04");
+                        lineGen_04.css({"top": "71px"});
+                        lineGen_04.animate({"width": "9px", "height": "9px", "opacity": "1"}, 150, "easeInOutBack", function() {
+                            $("main").append("<div id='lineGen_05'></div>");
+                            var lineGen_05 = $("#lineGen_05");
+                            lineGen_05.css("top", "75px");
+                            lineGen_05.animate({"opacity": "1"}, 150, "easeInOutQuart", function() {
+                                $("main").append("<div id='lineGen_06'></div>");
+                                var lineGen_06 = $("#lineGen_06");
+                                lineGen_06.css({"top": "71px"});
+                                lineGen_06.animate({"width": "9px", "height": "9px", "opacity": "1"}, 150, "easeInOutBack", function() {
+
+                                });
+                            });
+                        });
+                    });
+                    
+                });
+                animSequence = false;
+            });
+        }
+        else
+        {
+            if (shouldReset == true)
+            {
+                var lineGen_01 = $("#lineGen_01");
+                lineGen_01.animate({"width": "0px", "opacity": "0.3"}, 150, "easeInOutQuart", function() {
+                    animSequence = false;
+                });
+                $("main").remove(lineGen_01);
+            }
+            else
+            {
+                var lineGen_01 = $("#lineGen_01");
+                lineGen_01.animate({"width": "0px", "opacity": "0.3"}, 150, "easeInOutQuart", function() {
+                    lineGen_01.css("top", objLocation.toString() + "px")
+                    lineGen_01.animate({"width": "75px", "opacity": "1"}, 300, "easeInOutQuart", function() {
+                        animSequence = false;
+                    });
+                });
+            }
+        }
     }
     
     htmldrop.click(function() {
